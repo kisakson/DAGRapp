921af19c2a5efabdada85887b4d512d5a016199f
Kara Isakson <kara.isakson@gmail.com>
2017-04-26 09:40:58 -0400

    Adding basic php structure

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<?php    
+    // load up your config file
+    require_once("/../../resources/config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
+<div id='main-container'>
+    <div id='main-content'>
+
+        <!-- content -->
+        
+    </div>
+</div>
+<?php
+    require_once(TEMPLATES_PATH . "/footer.php");
+?>
---
+++ resources/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ 
+<html lang="en">
+  <head>
+    <title>DAGR Saver</title>
+  </head>
+ 
+  <body>
+    <div id='header'>
+      <h1>DAGR Saver</h1>
+      <ul class="nav global">
+        <li>list 1</li>
+        <li>list 2</li>
+      </ul>
+    </div>
+    
