b6893332109b33976191554920f76acd98ec718c
Kara Isakson <kara.isakson@gmail.com>
2017-04-30 22:07:22 -0400

    Current progress on search with get request and no page change.

--- public_html/php/searchdagr.php
+++ public_html/php/searchdagr.php
@@ -1,9 +1,11 @@
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="dagr-search-form" onsubmit="searchDagr()"> <!-- TODO check this action line -->
+<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="dagr-search-form"> <!-- TODO check this action line -->
 	DAGR Name: <input type="text" name="name"><br> <!-- TODO still in progress.. just creating the form structure -->
   Creator Name: <input type="text" name="creator"><br>
-  Contains Category: <input type="text" name="creator"></br>
-	<input type="submit" name="submit" value="Submit" />
+  Contains Category: <input type="text" name="category"></br>
+	<input type="submit" name="submit" value="Submit" id='dagr-search-button'/>
 </form>
+
+<script src="/js/body.js"></script>
 
 <div id='results'>
 </div>
@@ -12,16 +14,25 @@
 	include '../connect.php';
 
 	$db = db_connect();
-	$name = $_GET['q'];
-	$stmt = $db->prepare("SELECT * FROM `DAGR` WHERE `Name` LIKE ?");
+	$name = $_GET['name'];
+	$creator = $_GET['creator'];
+	$cat = $_GET['cat'];
+	$stmt = $db->prepare("SELECT * FROM `DAGR` WHERE `Name` LIKE ? AND 'Creator' LIKE ?");
 	
-	@$stmt->bind_param('s', $name)
+	@$stmt->bind_param('ss', $name, $creator)
 	OR die('Could not connect. .. . .. .');
 	
 	$stmt->execute();
 	$stmt->bind_result($col1, $col2, $col3, $col4, $col5);
 
-	print '<table border="5">';
+	$output = '<table border="5"><tr><td>Guid</td><td>Name</td><td>Creator</td><td>Time_created</td><td>Parent_id</td></tr>';
+	while($stmt->fetch()) {
+		$output = $output . '<tr><td>'.$col1.'</td><td>'.$col2.'</td><td>'.$col3.'</td><td>'.$col4.'</td><td>'.$col5.'</td></tr>';
+	}
+	$output = $output . '</table>';
+	console.log($output);
+
+	/*print '<table border="5">';
 	print '<tr>';
 	print '<td> GUID </td>';
 	print '<td> Name </td>';
@@ -41,8 +52,8 @@
 	    print '</tr>';
 
 	}   
-	print '</table>';
+	print '</table>';*/
 	
 	/* close statement */
-    	$stmt->close();
+  $stmt->close();
 ?>
