6e954b1406b7056edd76028d6d8a505473f60319
Kara Isakson <kara.isakson@gmail.com>
2017-05-05 22:58:59 -0400

    Create structure/outline for add file.

--- public_html/js/add.js
+++ public_html/js/add.js
@@ -1,9 +1,27 @@
 $('#dagr-add-button').on('click', function(e) {
+  // TODO do a check to see if any input values are missing
   e.preventDefault();
     $.ajax({
         url : '/php/responses/add.php',
         type: "POST",
         data: $('#dagr-add-form').serialize(),
+        success: function (result) {
+            $("#results").html(result);
+        },
+        error: function (jXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+  $("#results").html('<p>Adding...</p>');
+});
+
+$('#file-add-button').on('click', function(e) {
+  // TODO do a check to see if any input values are missing
+  e.preventDefault();
+    $.ajax({
+        url : '/php/responses/add.php',
+        type: "POST",
+        data: $('#file-add-form').serialize(),
         success: function (result) {
             $("#results").html(result);
         },
--- public_html/php/addfile.php
+++ public_html/php/addfile.php
@@ -1,23 +1,38 @@
-<table border="0">
-  <tbody>
-    <tr>
-      <td>
-        <h2>Add a File</h2>
-        <p>This page adds a new File within a DAGR. This DAGR can be created alone or within another existing DAGR.</p>
-        <!-- A user may create a DAGR with the components of the web page, or add these components to an open -->
-        <!--
-          <form action="#" method="post">
-		      <select name=dropdown>
-		        <option value="select">Select</option>
-		        <option value="insert">Insert</option>
-		        <option value="search">Search</option>
-		        <option value="view">View All</option>
-	        </select>
-	        <input type="submit" name="submit" value="Submit" />
-		      </form> -->
-        <p>... form to add a File will be below ...</p>
+<h2>Add a File</h2>
+<p>This page adds a new File within a DAGR. This containing DAGR can be created alone or within another existing DAGR.</p>
+<p>To save a file in the database, select the option above to add a new file.</p>
+        
+<form method="post" action="<?php echo htmlspecialchars('php/responses/add.php');?>" id="file-add-form">
+	File Name: <input type="text" name="name"><br>
+  Creator Name: <input type="text" name="creator"><br>
+  <select name="localoronline">
+    <option value="null"></option>
+    <option value="local">Local</option>
+    <option value="online">Online</option>
+  </select></br>
+  <!-- if local clicked: add an upload thing here
+        if online clicked: add a textbox here -->
+  <!-- find the file size -->
+  <!-- get the file type from the url value above -->
+  DAGR Parent: <select name="parent">
+    <option value="null"></option>
+    <option value="new">New DAGR</option>
+    <?php
+      include '../connect.php';
 
-      </td>
-    </tr>
-  </tbody>
-</table>
+      $db = db_connect();
+	    $stmt = $db->prepare("SELECT * FROM `DAGR` ORDER BY `Name`");
+      $stmt->execute();
+	    $stmt->bind_result($col1, $col2, $col3, $col4, $col5);
+      while($stmt->fetch()) {
+        echo '<option value="', $col1, '">', $col2, ' - id: ', $col1, '</option>';
+      }
+    ?>
+  </select><br>
+  <input type="hidden" name="object" value="file">
+	<input type="submit" name="submit" value="Submit" id='file-add-button'/>
+</form>
+
+<script src="/js/add.js"></script>
+
+<div id='results'></div>
